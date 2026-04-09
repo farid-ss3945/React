887cc80bc0efc7a4f3d0697f4a6aac75e147c2bb
Farid Abbasli <abbaslifarid2006@gmail.com>
2026-04-09 15:30:20 +0400

    Delete Project/src directory

--- Project/src/Redux/utils.jsx
+++
@@ -1,6 +0,0 @@
-export const loadUserData = (userId) => {
-    return {
-        cart: JSON.parse(localStorage.getItem(`cart_${userId}`)) || { items: [] },
-        favorites: JSON.parse(localStorage.getItem(`favorites_${userId}`)) || { items: [] },
-    };
-};
